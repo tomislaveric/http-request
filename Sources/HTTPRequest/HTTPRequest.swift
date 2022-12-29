a12e00c47977d54309d1c181a42c750150821359
tomislav <hi@tomislav.mobi>
2022-12-29 08:03:27 +0100

    add post function without body

--- Sources/HTTPRequest/HTTPRequest.swift
+++ Sources/HTTPRequest/HTTPRequest.swift
@@ -4,6 +4,7 @@ public protocol HTTPRequest {
     func get<ReturnType: Decodable>(request: URLRequest) async throws -> ReturnType
     func post<ReturnType: Decodable, BodyType: Encodable>(request: URLRequest, body: BodyType?) async throws -> ReturnType
     func post<BodyType: Encodable>(request: URLRequest, body: BodyType?) async throws
+    func post(request: URLRequest) async throws
 }
 
 public struct HTTPRequestImpl: HTTPRequest {
@@ -21,10 +22,17 @@ public struct HTTPRequestImpl: HTTPRequest {
         return try await handleRequest(request: request)
     }
     
-    public func post<BodyType: Encodable>(request: URLRequest, body: BodyType? = nil) async throws {
+    public func post<BodyType: Encodable>(request: URLRequest, body: BodyType) async throws {
         var request = request
         request.httpMethod = "POST"
         request.httpBody = try JSONEncoder().encode(body)
+        
+        try await handleRequest(request: request)
+    }
+    
+    public func post(request: URLRequest) async throws {
+        var request = request
+        request.httpMethod = "POST"
         try await handleRequest(request: request)
     }
     
