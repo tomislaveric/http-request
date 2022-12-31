79f1b0eace76669b68adf5d0f50a8d4325bc4561
tomislav <hi@tomislav.mobi>
2022-12-31 10:21:24 +0100

    set iOS min target to 15

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "http-request",
     platforms: [
         .macOS(.v10_15),
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
